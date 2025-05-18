6c94a1e7477505d51e044074c5d13d226af52497
ZAWG <freakzzghost@gmail.com>
2025-05-18 04:19:49 +0300

    more changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,99 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import * as React from 'react';
 import Button from '@mui/material/Button';
-import img from '../logo-dark.png';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import Switch from '@mui/material/Switch';
+
+// icon imports
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+
+
+function ThemeToggler() {
+    const [alignment, setAlignment] = React.useState('left');
+
+    const handleAlignment = (event, newAlignment) => {
+        setAlignment(newAlignment);
+    };
+
+    return (
+        <ToggleButtonGroup 
+            value={alignment}
+            exclusive
+            onChange={handleAlignment}
+            aria-label='text alignment'
+        >
+            <ToggleButton value='left' aria-label'left aligned'>
+        </ToggleButtonGroup>
+    )
+}
+
+function BarDrawer() {
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
+
+    const DrawerContent = (
+        <Box sx={{ width: 250 }} role='presentation' onClick={toggleDrawer(false)}>
+            <List>
+                {['ΑΡΧΙΚΗ', 'Ο ΛΟΓΑΡΙΑΣΜΟΣ ΜΟΥ'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <HomeRoundedIcon /> : <AccountCircleRoundedIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    return (
+        <div>
+            <Button onClick={toggleDrawer(true)}>Open</Button>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerContent}
+            </Drawer>
+        </div>
+    );
+}
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
             <div className='content-left'>
-                <a href='#' className='test'>
-                   test 
-                </a>
+                <BarDrawer />
             </div>
             <div className='content-center'>
-                <img className='bar-logo' src={img} href='#' />
+                <a href='#'>Panepistimiakos</a>
             </div>
             <div className='content-right'>
                 <Button className='bar-button' variant="outlined">Sign In</Button>
